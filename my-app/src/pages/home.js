2c1b39b0e6cc259f11fbd357e3342053e3dcdccf
swanndolia <swanndolia@gmail.com>
2023-02-14 05:18:09 +0100

    saved images + shitty card generation

--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,5 +1,20 @@
+import accomodations from "../assets/json/accomodation.json"
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return generateCards()
+};
+
+function generateCards(){
+  const accomodationsCards = []
+  accomodations.forEach(accomodation => {
+    accomodationsCards.push(
+    <div>
+      <h1>{accomodation.title}</h1>
+      <img src={accomodation.cover}></img>
+    </div>
+    )
+  });
+  return accomodationsCards;
 };
 
 export default Home;
