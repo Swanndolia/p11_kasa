0183e4354e9bdf0f0e67e533f6e8402827de380d
swanndolia <swanndolia@gmail.com>
2023-02-27 06:51:55 +0100

    TODO more css & carousel, done beside that

--- my-app/src/components/card_home.js
+++ my-app/src/components/card_home.js
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-const CardsHome = ({accomodation}) => {
+const CardsHome = ({ accomodation }) => {
     const navigate = useNavigate();
     return (
-        <figure onClick={() => navigate("/accomodation/" + accomodation.id )}>
+        <figure onClick={() => navigate("/accomodation/" + accomodation.id, { state: accomodation })}>
             <figcaption>{accomodation.title}</figcaption>
             <img src={accomodation.cover} alt="cover"></img>
         </figure>)
--- my-app/src/pages/accomodation.js
+++ my-app/src/pages/accomodation.js
@@ -1,5 +1,28 @@
-const Accomodation = (accomodation) => {
-    return <figure><figcaption></figcaption></figure>;
-  };
-  
-  export default Accomodation;
+import { useLocation } from 'react-router-dom';
+import Collapse from '../components/collapse';
+
+const Accomodation = () => {
+  const accomodation = useLocation().state
+  return (
+    <>
+      <figure>
+        <img src={accomodation.cover} alt="cover"></img>
+        <figcaption>
+          <h1>{accomodation.title}</h1>
+          <h2>{accomodation.location}</h2>
+        </figcaption>
+      </figure>
+      <div>{accomodation.tags.map(tag => (<span>{tag}</span>))}</div>
+      <Collapse
+        trigger="Description"
+        content={<p>{accomodation.description}</p>}
+      />
+      <Collapse
+        trigger="Equipements"
+        content={<ul>{accomodation.equipments.map(equipement => (<li>{equipement}</li>))}</ul>}
+      />
+    </>
+  );
+};
+
+export default Accomodation;
